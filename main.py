d6b48a6f60b817d2cefbce7b5b47dcdf7357c4ee
KleighKennnedy <150048922+KleighKennnedy@users.noreply.github.com>
2023-11-06 15:23:06 +0000

    Basic Setup
    I set up a base document/file system that I can use without significant modification. I will need to incorporate a separate JS file under 'static'.

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from types import BuiltinFunctionType
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
-  
+    return render_template('test.html')
+
+
+@app.route('/hi')
+def hi():
+    return render_template('thisthing.html')
+
+
+@app.route('/beep')
+def beep():
+    return """
+     <script>
+   
+      alert('BEEP');
+     
+    """
+
 ###ignore
 
 app.run(host='0.0.0.0', port=81)
