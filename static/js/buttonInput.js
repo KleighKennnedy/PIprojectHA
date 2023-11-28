cacd8490f2181f3bad759e24c9f4f41a24352981
KleighKennnedy <150048922+KleighKennnedy@users.noreply.github.com>
2023-11-28 08:19:01 -0500

    Button Input Test IV

--- static/js/buttonInput.js
+++ static/js/buttonInput.js
@@ -5,7 +5,7 @@ setInterval(function() {
             // Update the client-side based on the server response
             if (data.state === '1') {
                 // Button is pressed
-                alert('Button is pressed');
+                console.log('Button is pressed');
             } else {
                 // Button is not pressed
                 ('Button is not pressed');
